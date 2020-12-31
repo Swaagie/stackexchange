cf3bf682c3e279aea81711d5c8ed8230335f1c6b
Rich Trott <rtrott@gmail.com>
2020-12-31 10:31:22 -0800

    fix: fix error message for questions.downvote()
    Error message for downvote with  missing key and/or access token
    incorrectly identified itself as for upvote.

--- test/questions-test.js
+++ test/questions-test.js
@@ -18,18 +18,30 @@ describe('Questions', function () {
     };
   });
 
-  it('throws if no callback', function(done) {
+  it('throws if no callback', function() {
     expect(() => { context.questions.questions(filter); }).to.throw();
-    done();
+    expect(() => { context.questions.answers(filter); }).to.throw();
   })
 
-  it('get questions', function(done) {
-    context.questions.questions(filter, function(err, results){
+  it('get questions and answers', function(done) {
+    context.questions.questions(filter, function(err, results) {
       if (err) throw err;
+
       expect(results.items).to.have.length(10);
       expect(results.has_more).to.be.true;
       expect(results.items.every((val) => val.link.startsWith('https://stackoverflow.com/questions/'))).to.be.true;
-      done();
+
+      const questionIds = results.items.map((item) => item.question_id);
+      const callback = (err, results) => {
+        if (err) throw err;
+
+        expect(results.items).to.have.length(10);
+        expect(results.has_more).to.be.true;
+        expect(results.items.every((val) => val.hasOwnProperty('answer_id'))).to.be.true;
+
+        done();
+      }
+      context.questions.answers(filter, callback, questionIds);
     });
   });
 
@@ -42,19 +54,19 @@ describe('Questions', function () {
       expect(results.items.every((val) => val.link.startsWith('https://softwareengineering.stackexchange.com/questions/'))).to.be.true;
       done();
     });    
-  })
+  });
 
-  it('upvote a question should throw an error without key or access_token', function(done) {
-    let count = 0;
-    const cases = [{}, {key: 'fhqwhgads'}, {access_token: 'fhqwhgads'}];
-    cases.forEach((filter) => {
-      context.questions.upvote(filter, '51812', (err) => {
-        expect(err.message).to.equal('questions.upvote lacks key and/or access token as criteria');
-        count++;
-        if (count >= cases.length) {
-          done();
-        }
+  it('upvote/downvote should throw an error without key or access_token', function() {
+    function check(fn) {
+      const cases = [{}, {key: 'fhqwhgads'}, {access_token: 'fhqwhgads'}];
+      cases.forEach((filter) => {
+        context.questions[fn](filter, '51812', (err) => {
+          expect(err.message).to.equal(`questions.${fn} lacks key and/or access token as criteria`);
+        });
       });
-    });
-  })
+    }
+
+    check('upvote');
+    check('downvote');
+  });
 });
