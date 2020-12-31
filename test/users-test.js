d1a1a645c6a06ed7cbfb7c4a31a1e6a270af35b7
Rich Trott <rtrott@gmail.com>
2020-12-31 08:44:40 -0800

    chore: complete test coverage for users.js

--- test/users-test.js
+++ test/users-test.js
@@ -1,3 +1,4 @@
+const { expect } = require('chai');
 var stackexchange = require('../lib/stackexchange');
 
 describe('Users', function () {
@@ -13,37 +14,52 @@ describe('Users', function () {
   });
 
   function expectUserProperty(item) {
-    expect(item).to.have.property('badge_counts');
-    expect(item).to.have.property('account_id');
-    expect(item).to.have.property('is_employee');
-    expect(item).to.have.property('last_modified_date');
-    expect(item).to.have.property('last_access_date');
-    expect(item).to.have.property('reputation_change_year');
-    expect(item).to.have.property('reputation_change_quarter');
-    expect(item).to.have.property('reputation_change_month');
-    expect(item).to.have.property('reputation_change_week');
-    expect(item).to.have.property('reputation_change_day');
-    expect(item).to.have.property('reputation');
-    expect(item).to.have.property('creation_date');
-    expect(item).to.have.property('user_type');
-    expect(item).to.have.property('user_id');
-    expect(item).to.have.property('accept_rate');
-    expect(item).to.have.property('location');
-    expect(item).to.have.property('website_url');
-    expect(item).to.have.property('link');
-    expect(item).to.have.property('profile_image');
-    expect(item).to.have.property('display_name');
+    const props = ['badge_counts', 'account_id', 'is_employee',
+                   'last_modified_date', 'last_access_date',
+                   'reputation_change_year', 'reputation_change_quarter',
+                   'reputation_change_month', 'reputation_change_week',
+                   'reputation_change_day', 'reputation', 'creation_date',
+                   'user_type', 'user_id', 'location', 'website_url', 'link',
+                   'profile_image', 'display_name',];
+    props.forEach((prop) => expect(item).to.have.property(prop));
   }
 
-  it('get users', function(done) {
+  function expectedAnswerProperty(item) {
+    const props = ['owner', 'is_accepted', 'score', 'last_activity_date',
+                   'creation_date', 'answer_id', 'question_id',
+                   'content_license',];
+    props.forEach((prop) => expect(item).to.have.property(prop));
+  }
+
+  it('get users and get answers for user', function(done) {
     context.users.users(filter, function(err, results) {
       if (err) throw err;
 
       expect(results.items).to.have.length(10);
-      expectUserProperty(results.items[0]);
+      results.items.forEach((item) => { expectUserProperty(item); });
       expect(results.has_more).to.be.true;
-      done();
+
+      const userIds = results.items.map((items) => items.user_id);
+
+      context.users.answers(filter, userIds, function(err, results) {
+        if (err) throw err;
+
+        expect(results.items).to.have.length(10);
+        results.items.forEach((item) => { expectedAnswerProperty(item); });
+        expect(results.has_more).to.be.true;
+
+        done();
+      });
     });
   });
 
+  it('throws if answers does not get an array of ids', function(done) {
+    function checkError(err) {
+      expect(err).to.be.instanceof(Error)
+      expect(err.message).to.equal('users.answers lacks IDs to query');
+    }
+
+    context.users.answers(filter, 0, checkError);
+    context.users.answers(filter, [], (err) => { checkError(err); done(); });
+  });
 });
