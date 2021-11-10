722ac65c1d83840ef2779afdd1e51568550ca646
Rich Trott <rtrott@gmail.com>
2021-11-10 08:38:57 -0800

    chore: remove nconf dependency
    We can use a Map instead of nconf.

--- test/config-test.js
+++ test/config-test.js
@@ -1,29 +1,25 @@
-/*global expect*/
-var config = require('../lib/config');
+/* global expect */
+const config = require('../lib/config')
 
 describe('Config', function () {
-  'use strict';
+  'use strict'
 
-  it('has API key', function() {
-    expect(config.get('api')).to.equal('api.stackexchange.com');
-  });
+  it('has API key', function () {
+    expect(config.get('api')).to.equal('api.stackexchange.com')
+  })
 
   it('has API endpoint', function () {
-    expect(config.get('site')).to.equal('stackoverflow');
-  });
+    expect(config.get('site')).to.equal('stackoverflow')
+  })
 
   it('has API version', function () {
-    var version = config.get('version');
+    const version = config.get('version')
 
-    expect(version).to.equal(2.2);
-    expect(version).to.be.a('Number');
-  });
+    expect(version).to.equal(2.2)
+    expect(version).to.be.a('Number')
+  })
 
   it('has default protocol', function () {
-    expect(config.get('protocol')).to.equal('https:');
-  });
-
-  it('uses memory storage', function () {
-    expect(config.stores).to.have.property('memory');
-  });
-});
+    expect(config.get('protocol')).to.equal('https:')
+  })
+})
